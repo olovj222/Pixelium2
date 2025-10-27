e103db961d7f82a615106836f75c129a1235cd0f
olovj222 <ol.juengt@duocuc.cl>
2025-10-27 11:05:49 -0300

    Delete GameVerseAndroid directory

--- GameVerseAndroid/app/src/main/java/com/gameverse/ui/theme/Color.kt
+++
@@ -1,9 +0,0 @@
-package com.gameverse.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val NeonBlue = Color(0xFF00CFFF)
-val DarkBackground = Color(0xFF000000)
-val DarkSurface = Color(0xFF101010)
-val TextPrimary = Color(0xFFE0E0E0)
-val TextSecondary = Color(0xFFFDFCFC)
